1d197f276514a3a2ee051c396781afdd20f894f6
duncanfd24 <159070352+duncanfd24@users.noreply.github.com>
2024-07-18 13:52:49 -0400

    Added About, Contact, Home, Portfolio, and Resume pages.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,8 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      My name is Fred Duncan, and I am eager to begin my journey in web development.  I began the EdX full stack web development program
+      in February of 2024, and am learning something new everyday.  Please take some time to review some of my work, and feel free to reach out with any questions.
       </p>
     </div>
   );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,49 @@
-import Project from "../components/Project";
+import React from 'react';
+import Project from '../components/Project';
 
-export default function Portfolio() {
+const projects = [
+  {
+    title: 'Weathering the Dashboard',
+    description: 'Search any city and discover its five day forecast.',
+    link: 'https://duncanfd24.github.io/Weatheringthedashboard/',
+    imageUrl: 'https://cdn.wallpapersafari.com/69/45/lF1thv.jpg'
+  },
+  {
+    title: 'Employee Payroll Data Management',
+    description: 'Tool to manage your payroll data.',
+    link: 'https://duncanfd24.github.io/Optimizing-Management-of-Employee-Payroll-Data/',
+    imageUrl: 'https://img.freepik.com/free-photo/top-view-payroll-concept-with-wooden-cubes_23-2149103955.jpg?t=st=1721262727~exp=1721266327~hmac=c00e8a5e686496f5184a8bb0e7d1a6c32d30b1a961e1ee699cce1c2c698e5fc5&w=900'
+  },
+  {
+    title: 'Movie Night',
+    description: 'Choose your next movie choice easily.',
+    link: 'https://tawatson02.github.io/movie-night/',
+    imageUrl: 'https://media.istockphoto.com/id/1396163273/vector/vector-realistic-isolated-retro-marquee-neon-billboard-with-electric-light-lamps-of-movie.jpg?s=612x612&w=0&k=20&c=8Uy2KIp0x7OlJIhkEOV5Pbb0vTKs7dnvfjCa7A1K3tc='
+  },{
+    title: 'NutriFit',
+    description: 'Keep track of your nutrition and fitness all in one application.',
+    link: 'https://github.com/rvanetta97/NutriFit',
+    imageUrl: 'https://www.chirothinweightloss.com/wp-content/uploads/2018/11/exercise-vs-diet.jpeg'
+  }, 
+];
+
+function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio Page</h1>
-      <Project/>
+    <div className="portfolio">
+      <h2>My Portfolio</h2>
+      <div className="projects">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+            imageUrl={project.imageUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default Portfolio;
